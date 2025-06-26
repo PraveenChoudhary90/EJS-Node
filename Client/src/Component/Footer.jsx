97f5fcdf6ee4b332b56e99da9d7f11b3d62b221c
praveen choudhary <pc852180@gmail.com>
2025-06-26 15:10:47 +0530

    Data

---
+++ Client/src/Component/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <>
+    <h1 style={{padding:"90px", backgroundColor:"lightpink", textAlign:"center", fontWeight:"bold"}}>www.mycompany.com</h1>
+    </>
+  )
+}
+
+export default Footer
---
+++ Client/src/Component/TopNav.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function TopNav() {
+  return (
+    <>
+    
+    
+    
+    </>
+  )
+}
+
+export default TopNav
